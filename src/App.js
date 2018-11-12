ab43ea4afde2b21c60f3ee3e9a39977298b6d877
KellenCheng <kken339039@yahoo.com.tw>
2018-11-12 20:59:47 +0800

    Init front-end packe default view

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Index = () => {
+  return <div></div>
+}
+
+const Daily = () => {
+  return <div></div>
+}
+
+const Navigation = () => {
+  return <div className="navigation">
+    <ul className="nav">
+      <li>
+        <Link to={'/'} className="nav-link" >
+          <span className="">列車查詢</span>
+        </Link>
+      </li>
+      <li>
+        <Link to={'/daily'} className="nav-link" >
+          <span className="">當日查詢</span>
+        </Link>
+      </li>
+    </ul>
+  </div>
+}
+
+const UserForm = () => {
+  return <div className="user-form">
+    </div>
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Router>
+        <div className="app">
+          <div className="app-body">
+            <div className="side-bar">
+              <Navigation />
+            </div>
+            <div className="contain-block">
+              <UserForm />
+              <div className="card-block">
+                <Route exact path="/" component={Index} />
+                <Route exact path="/daily" component={Daily} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Router>
   }
 }
 
